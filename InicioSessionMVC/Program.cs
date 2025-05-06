3eeb58977ba35049269446f39a7664a5600cd86d
Alans369 <cs25001@esfe.agape.edu.sv>
2025-05-06 02:27:26 +0000

    add packaje facebook

--- InicioSessionMVC/Program.cs
+++ InicioSessionMVC/Program.cs
@@ -1,5 +1,14 @@
 var builder = WebApplication.CreateBuilder(args);
 
+var services = builder.Services;
+var configuration = builder.Configuration;
+
+services.AddAuthentication().AddFacebook(facebookOptions =>
+    {
+        facebookOptions.AppId = configuration["Authentication:Facebook:AppId"];
+        facebookOptions.AppSecret = configuration["Authentication:Facebook:AppSecret"];
+    });
+
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
